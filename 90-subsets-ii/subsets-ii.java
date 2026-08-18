fe624011dce16e3856a9bac2fd7c6125e2cd080b
Aman Prajapati <amanpr6860@gmail.com>
2026-08-18 16:25:34 +0530

    Time: 3 ms (54.76%) | Memory: 44.7 MB (95.23%) - LeetSync

--- 90-subsets-ii/subsets-ii.java
+++ 90-subsets-ii/subsets-ii.java
@@ -1,23 +1,18 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
-        
         // Sort the array to easily identify duplicates
         Arrays.sort(nums);
-        
         // Start backtracking
         backtrack(result, new ArrayList<>(), nums, 0);
         return result;
     }
-    
     private void backtrack(List<List<Integer>> result, List<Integer> currentSubset, int[] nums, int start) {
         // Add a copy of the current valid subset
         result.add(new ArrayList<>(currentSubset));
-        
         for (int i = start; i < nums.length; i++) {
             // Skip duplicates: 
             // If it's not the first element in this loop iteration (i > start) 
@@ -25,13 +20,10 @@ class Solution {
             if (i > start && nums[i] == nums[i - 1]) {
                 continue;
             }
-            
             // Choose
             currentSubset.add(nums[i]);
-            
             // Explore
             backtrack(result, currentSubset, nums, i + 1);
-            
             // Un-choose (Backtrack)
             currentSubset.remove(currentSubset.size() - 1);
         }
