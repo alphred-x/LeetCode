f6ceba6313339224e3740f4e2cdbc76ac3d31aee
Aman Prajapati <amanpr6860@gmail.com>
2026-08-22 10:27:40 +0530

    Time: 2 ms (99.73%) | Memory: 45.3 MB (24.10%) - LeetSync

--- 90-subsets-ii/subsets-ii.java
+++ 90-subsets-ii/subsets-ii.java
@@ -1,31 +1,25 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 class Solution {
-    public List<List<Integer>> subsetsWithDup(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        // Sort the array to easily identify duplicates
-        Arrays.sort(nums);
-        // Start backtracking
-        backtrack(result, new ArrayList<>(), nums, 0);
-        return result;
-    }
-    private void backtrack(List<List<Integer>> result, List<Integer> currentSubset, int[] nums, int start) {
-        // Add a copy of the current valid subset
-        result.add(new ArrayList<>(currentSubset));
-        for (int i = start; i < nums.length; i++) {
-            // Skip duplicates: 
-            // If it's not the first element in this loop iteration (i > start) 
-            // and it's equal to the previous element, skip to avoid duplicate subsets.
-            if (i > start && nums[i] == nums[i - 1]) {
-                continue;
-            }
-            // Choose
-            currentSubset.add(nums[i]);
-            // Explore
-            backtrack(result, currentSubset, nums, i + 1);
-            // Un-choose (Backtrack)
-            currentSubset.remove(currentSubset.size() - 1);
+    void fun(List<Integer> input,List<Integer> output,List<List<Integer>> ans){
+        ans.add(new ArrayList<>(output));
+        if(input.size()==0) return;
+        for(int i=0;i<input.size();i++){
+            List<Integer> ip=new ArrayList<>(input);
+            List<Integer> op=new ArrayList<>(output);
+            if(i>0 && ip.get(i)==ip.get(i-1)) continue;
+            op.add(ip.get(i));
+            ip.subList(0,i+1).clear();
+            fun(ip,op,ans);
         }
+    }
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> ans=new ArrayList<>();
+        List<Integer> input=new ArrayList<>();
+        List<Integer> output=new ArrayList<>();
+        for(int a:nums){
+            input.add(a);
+        }
+        Collections.sort(input);
+        fun(input,output,ans);
+        return ans;
     }
 }
